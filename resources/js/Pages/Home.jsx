0eac97915c009d4a5448c4270178a6933782dee0
unknown <emrekaradereli0@gmail.com>
2024-05-11 22:43:36 +0300

    message input layout done

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -5,6 +5,7 @@ import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
 import { useState, useEffect, useRef } from "react";
 import ConversationHeader from "@/Components/App/ConversationHeader";
 import MessageItem from "@/Components/App/MessageItem";
+import MessageInput from "@/Components/App/MessageInput";
 
 function Home({ selectedConversation = null, messages = null }) {
 
@@ -58,7 +59,7 @@ function Home({ selectedConversation = null, messages = null }) {
                             </div>
                         )}
                     </div>
-                   {/*  <MessageInput conversation={selectedConversation} /> */}
+                  <MessageInput conversation={selectedConversation} />
                 </>
             )}
         </>
