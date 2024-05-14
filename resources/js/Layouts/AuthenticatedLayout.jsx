5ca0f9be6b7a017678e4e2a5c3706eaccab2adef
unknown <emrekaradereli0@gmail.com>
2024-05-14 20:56:02 +0300

    eventbust (not done)

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -5,12 +5,14 @@ import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link, usePage } from '@inertiajs/react';
 import { useEffect } from 'react';
+import { useEventBus } from '@/EventBus';
 
 export default function Authenticated({ header, children }) {
     const page = usePage();
     const user = page.props.auth.user;
     const conversations = page.props.conversations;
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
+    const { emit } = useEventBus();
 
     useEffect(() => {
         conversations.forEach((conversation) => {
@@ -32,20 +34,22 @@ export default function Authenticated({ header, children }) {
                 console.log("SocketMessage", e);
                 const message = e.message;
 
-                // emit("message.created", message);
+                emit("message.created", message);
                 if(message.sender_id === user.id) {
                     return;
                 }
-                /* emit("newMessageNotification", {
+                emit("newMessageNotification", {
                     user: message.sender,
                     group_id: message.group_id,
-                    message: message.message || `Shared $ {
-                        message.attachments.length === 1 ?  "an attachment" : message.attachments.length + "attachments"
+                    message:
+                    message.message ||
+                     `Shared ${
+                        message.attachments.length === 1 ?  "an attachment" : message.attachments.length + " attachments"
                     }`,
                 });
-                */
-            })
-        })
+
+            });
+        });
 
         return () => {
 
