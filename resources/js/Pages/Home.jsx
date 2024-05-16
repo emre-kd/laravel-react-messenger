27dc743cf8abbdfe245fe84a37b29466fedcfd79
unknown <emrekaradereli0@gmail.com>
2024-05-16 22:13:00 +0300

    load older message start

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -76,6 +76,7 @@ function Home({ selectedConversation = null, messages = null }) {
                         )}
                         {localMessages.length > 0 && (
                             <div className="flex-1 flex flex-col">
+                            <div ref={loadMoreIntersect}></div>
                                 {localMessages.map((message) => (
                                     <MessageItem
                                         key={message.id}
